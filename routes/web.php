bf346385a65e9cf33947512a78f007bdd92a41c1
Jorge-LCG <jorgeluiscg2003@gmail.com>
2025-10-21 16:34:09 -0500

    Nuevas rutas de autenticación

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\Auth\ForgotPassowrdController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ResetPasswordController;
 use App\Http\Controllers\Post\PostController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -21,6 +23,12 @@ Route::middleware(['guest', 'web'])->prefix('auth')->group(function () {
 
     Route::get('/registrar-cuenta', [RegisterController::class, 'index'])->name('register');
     Route::post('/registrar-cuenta', [RegisterController::class, 'store'])->name('register.store');
+
+    Route::get('/olvide-contraseña', [ForgotPassowrdController::class, 'index'])->name('password');
+    Route::post('/olvide-contraseña', [ForgotPassowrdController::class, 'store'])->name('password.store');
+
+    Route::get('/recuperar-contraseña/{token}', [ResetPasswordController::class, 'index'])->name('password.reset');
+    Route::post('/recuperar-contraseña', [ResetPasswordController::class, 'store'])->name('password.update');
 });
 
 Route::middleware(['auth', 'web'])->group(function () {
